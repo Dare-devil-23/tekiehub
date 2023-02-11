bc04349f398e8d343f4e61f2f2bb56d0d2d28874
Dare-devil-23 <sahithmummy22@gmail.com>
2023-02-12 00:50:10 +0530

    careers updated with search

---
+++ src/asserts/jobsData.js
@@ -0,0 +1,23 @@
+const JobsData = [
+    {
+      id: 1,
+      role: "Salesforce Developer/Architect",
+      location: "Boston, MA",
+  },
+    {
+      id: 2,
+      role: "Salesforce Administrator",
+      location: "Englewood, CO",
+  },
+    {
+      id: 3,
+      role: "Salesforce Developer/Architect",
+      location: "Boston, MA",
+    },
+    {
+      id: 4,
+      role: "Sr JAVA FULL STACK DEVELOPER",
+      location: "Remote",
+    },
+  ];
+  export default JobsData;
--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,14 +1,37 @@
-import { Card } from 'antd';
+import { Card, Empty, Modal } from 'antd';
 import Input from 'antd/lib/input/Input';
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { IoIosArrowDown } from 'react-icons/io'
+import JobsData from '../asserts/jobsData';
 
 const Careers = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchProp, setSearchProp] = useState("");
+  const [openJob, setOpenJob] = useState(false);
+
   const handleScroll = () => {
     const element = document.getElementById("content");
     element.scrollIntoView({ behavior: "smooth" });
   };
+
+  const results = JobsData
+    .filter((val) => {
+      if (searchTerm === "") {
+        return val;
+      } else if (
+        searchProp === "role" &&
+        val?.role.toLowerCase().includes(searchTerm.toLowerCase())
+      ) {
+        return val;
+      } else if (
+        searchProp === "location" &&
+        val?.location.toLowerCase().includes(searchTerm.toLowerCase())
+      ) {
+        return val;
+      }
+    })
+
   return (
     <div>
       <div
@@ -22,13 +45,10 @@ const Careers = () => {
             <div className="p-5 text-5xl">
               Check our current openings!!
             </div>
-            <p className='w-3/4 mx-auto text-lg'>
-              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.
-            </p>
           </div>
           <Link to="#" onClick={handleScroll}>
-              <IoIosArrowDown className='text-[40px] animate-bounce text-white hover:animate-none hover:scale-110 font-thin cursor-pointer' />
-            </Link>
+            <IoIosArrowDown className='text-[40px] animate-bounce text-white hover:animate-none hover:scale-110 font-thin cursor-pointer' />
+          </Link>
         </div>
       </div>
 
@@ -37,25 +57,71 @@ const Careers = () => {
         <div className='flex justify-between flex-col lg:flex-row'>
           <div className='w-full lg:w-1/2 mx-5'>
             <h3 className='text-xl my-3'>Search for</h3>
-            <Input placeholder='Job tittle or keyword' size='large' className='w-10/12' />
+            <Input
+              placeholder='Job tittle or keyword'
+              size='large'
+              className='w-10/12'
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+                setSearchProp("role");
+              }}
+            />
           </div>
           <div className='w-full lg:w-1/2 mx-5'>
             <h3 className='text-xl my-3'>Where</h3>
-            <Input controls={false} placeholder='City state or zipcode' size='large' className='w-10/12' />
+            <Input
+              controls={false}
+              placeholder='City state or zipcode'
+              size='large'
+              className='w-10/12'
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+                setSearchProp("location");
+              }}
+            />
           </div>
         </div>
         <div className='mt-10'>
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-            <Card title="Card title" className='shadow-lg hover:bg-sky-100 bg-sky-50 min-h-[250px]'>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry
-            </Card>
-            <Card title="Card title" className='shadow-lg hover:bg-sky-100 bg-sky-50 min-h-[250px]'>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry
-            </Card>
-            <Card title="Card title" className='shadow-lg hover:bg-sky-100 bg-sky-50 min-h-[250px]'>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry
-            </Card>
-          </div>
+          {
+            results.length ?
+              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+                {
+                  results.map((job, i) => (
+                    <div key={i}>
+                      <Card
+                        title={job.role}
+                        className='shadow-lg hover:shadow-2xl'
+                        onClick={
+                          () => setOpenJob(true)
+                        }>
+                        {job.location}
+                      </Card>
+                      <Modal
+                        title={job.role}
+                        centered
+                        open={openJob}
+                        okText={"Apply Job"}
+                        okButtonProps={{ghost:true}}
+                        cancelText={"Close"}
+                        cancelButtonProps={{danger:true}}
+                        onOk={() => setOpenJob(false)}
+                        onCancel={() => setOpenJob(false)}
+                      >
+                        <p>{job.location}</p>
+                      </Modal>
+                    </div>
+                  ))}
+              </div> :
+              <Empty
+                className='w-full'
+                image={Empty.PRESENTED_IMAGE_SIMPLE}
+                description={
+                  <span>
+                    No Jobs Found.
+                  </span>
+                }
+              />
+          }
         </div>
       </div>
     </div>
